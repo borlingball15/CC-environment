3eabf9e84b72dd41cbee8d4db6d87d8c51b50f78
kwoncena <caraskwon@gmail.com>
2024-03-18 02:52:00 -0400

    arghghhg
    i hate arduinooooooooo added button

---
+++ CC Environment/Assets/ButtonController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonController : MonoBehaviour
+{
+    private bool lightsOn = false;
+    private bool prevButtonState = false;
+
+    void Update()
+    {
+        if (UduinoManager.Instance.HasData(0))
+        {
+            string data = UduinoManager.Instance.ReadFromDevice(0);
+            bool buttonState = data.Trim() == "1";
+            if (buttonState != prevButtonState)
+            {
+                ToggleLights();
+                prevButtonState = buttonState;
+            }
+        }
+    }
+
+    void ToggleLights()
+    {
+        lightsOn = !lightsOn;
+        // Toggle your lights here
+        if (lightsOn)
+        {
+            Debug.Log("Lights On");
+            // Turn on the lights
+        }
+        else
+        {
+            Debug.Log("Lights Off");
+            // Turn off the lights
+        }
+    }
+}
